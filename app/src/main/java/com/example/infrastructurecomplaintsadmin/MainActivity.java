0050dbc80f3157c6fe2e1ac9a260538137b00656
Chetan Sharma <cschetan77@outlook.com>
2020-04-10 15:27:33 +0530

    Login Interface Complete

--- app/src/main/java/com/example/infrastructurecomplaintsadmin/MainActivity.java
+++ app/src/main/java/com/example/infrastructurecomplaintsadmin/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.infrastructurecomplaintsadmin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String choice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+        switch(view.getId()) {
+            case R.id.radioButton_admin :
+                if(checked) {
+                    choice = "Admin";
+                }
+                break;
+            case R.id.radioButton_dept :
+                if(checked) {
+                    choice = "Department";
+                }
+                break;
+
+        }
+        Toast.makeText(this, choice, Toast.LENGTH_SHORT).show();
     }
 }
